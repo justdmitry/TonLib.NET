f1d6b675832352e4f714d9c2ff506a4b1371e4d7
Dmitry Popov <dmitry@parsec-club.ru>
2023-02-03 01:04:46 +0300

    unused TypeBase.TypeName removed

--- TonLibDotNet/Types/TypeBase.cs
+++ TonLibDotNet/Types/TypeBase.cs
@@ -1,11 +1,7 @@
-﻿using System.Text.Json.Serialization;
-
-namespace TonLibDotNet.Types
+﻿namespace TonLibDotNet.Types
 {
     public abstract class TypeBase
     {
-        [JsonIgnore]
-        [Obsolete]
-        public string TypeName { get; protected set; } = string.Empty;
+        // Nothing
     }
 }
