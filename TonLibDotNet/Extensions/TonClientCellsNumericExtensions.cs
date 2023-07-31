530e92fe75ce31ea5a29569da975b37aad935677
Dmitry Popov <dmitry@parsec-club.ru>
2023-07-31 22:37:43 +0300

    new: PreloadUShort, PreloadShort, PreloadUInt, PreloadInt, PreloadULong, PreloadLong

--- TonLibDotNet/Extensions/TonClientCellsNumericExtensions.cs
+++ TonLibDotNet/Extensions/TonClientCellsNumericExtensions.cs
@@ -19,6 +19,18 @@ namespace TonLibDotNet
             return BinaryPrimitives.ReadUInt16BigEndian(data);
         }
 
+        public static ushort PreloadUShort(this Slice slice, int size = 16)
+        {
+            if (size < 1 || size > 16)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size));
+            }
+
+            Span<byte> data = stackalloc byte[2];
+            slice.PreloadBitsToBytesTo(size, data);
+            return BinaryPrimitives.ReadUInt16BigEndian(data);
+        }
+
         public static CellBuilder StoreUShort(this CellBuilder builder, ushort value, int size = 16)
         {
             if (size < 1 || size > 16)
@@ -40,6 +52,18 @@ namespace TonLibDotNet
 
             Span<byte> data = stackalloc byte[2];
             slice.LoadBitsToBytesTo(size, data);
+            return BinaryPrimitives.ReadInt16BigEndian(data);
+        }
+
+        public static short PreloadShort(this Slice slice, int size = 16)
+        {
+            if (size < 1 || size > 16)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size));
+            }
+
+            Span<byte> data = stackalloc byte[2];
+            slice.PreloadBitsToBytesTo(size, data);
             return BinaryPrimitives.ReadInt16BigEndian(data);
         }
 
@@ -71,6 +95,18 @@ namespace TonLibDotNet
             return BinaryPrimitives.ReadUInt32BigEndian(data);
         }
 
+        public static uint PreloadUInt(this Slice slice, int size = 32)
+        {
+            if (size < 1 || size > 32)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size));
+            }
+
+            Span<byte> data = stackalloc byte[4];
+            slice.PreloadBitsToBytesTo(size, data);
+            return BinaryPrimitives.ReadUInt32BigEndian(data);
+        }
+
         public static CellBuilder StoreUInt(this CellBuilder builder, uint value, int size = 32)
         {
             if (size < 1 || size > 32)
@@ -92,6 +128,18 @@ namespace TonLibDotNet
 
             Span<byte> data = stackalloc byte[4];
             slice.LoadBitsToBytesTo(size, data);
+            return BinaryPrimitives.ReadInt32BigEndian(data);
+        }
+
+        public static int PreloadInt(this Slice slice, int size = 32)
+        {
+            if (size < 1 || size > 32)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size));
+            }
+
+            Span<byte> data = stackalloc byte[4];
+            slice.PreloadBitsToBytesTo(size, data);
             return BinaryPrimitives.ReadInt32BigEndian(data);
         }
 
@@ -123,6 +171,18 @@ namespace TonLibDotNet
             return BinaryPrimitives.ReadUInt64BigEndian(data);
         }
 
+        public static ulong PreloadULong(this Slice slice, int size = 64)
+        {
+            if (size < 1 || size > 64)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size));
+            }
+
+            Span<byte> data = stackalloc byte[8];
+            slice.PreloadBitsToBytesTo(size, data);
+            return BinaryPrimitives.ReadUInt64BigEndian(data);
+        }
+
         public static CellBuilder StoreULong(this CellBuilder builder, ulong value, int size = 64)
         {
             if (size < 1 || size > 64)
@@ -144,6 +204,18 @@ namespace TonLibDotNet
 
             Span<byte> data = stackalloc byte[8];
             slice.LoadBitsToBytesTo(size, data);
+            return BinaryPrimitives.ReadInt64BigEndian(data);
+        }
+
+        public static long PreloadLong(this Slice slice, int size = 64)
+        {
+            if (size < 1 || size > 64)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size));
+            }
+
+            Span<byte> data = stackalloc byte[8];
+            slice.PreloadBitsToBytesTo(size, data);
             return BinaryPrimitives.ReadInt64BigEndian(data);
         }
 
