ebf8cad3f42ed03af8d98fd7bcd65a68e8d950e6
Dmitry Popov <dmitry@parsec-club.ru>
2023-11-27 22:47:42 +0300

    feat: add wallet.v4.*

---
+++ TonLibDotNet/Types/Wallet/V4AccountState.cs
@@ -0,0 +1,10 @@
+﻿namespace TonLibDotNet.Types.Wallet
+{
+    [TLSchema("wallet.v4.accountState wallet_id:int64 seqno:int32 = AccountState")]
+    public class V4AccountState : AccountState
+    {
+        public long WalletId { get; set; }
+
+        public int Seqno { get; set; }
+    }
+}
---
+++ TonLibDotNet/Types/Wallet/V4InitialAccountState.cs
@@ -0,0 +1,10 @@
+﻿namespace TonLibDotNet.Types.Wallet
+{
+    [TLSchema("wallet.v4.initialAccountState public_key:string wallet_id:int64 = InitialAccountState")]
+    public class V4InitialAccountState : InitialAccountState
+    {
+        public string PublicKey { get; set; } = string.Empty;
+
+        public long WalletId { get; set; }
+    }
+}
