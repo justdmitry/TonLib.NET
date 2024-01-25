86c7ab357bfdc2ee1031d097bdcfdf76d0a0f617
Dmitry Popov <dmitry@parsec-club.ru>
2024-01-25 13:43:17 +0300

    feat: AddressUtils.TrySetBounceable

--- TonLibDotNet/Utils/AddressUtils.cs
+++ TonLibDotNet/Utils/AddressUtils.cs
@@ -40,5 +40,25 @@ namespace TonLibDotNet.Utils
 
             return bounceable == bounceableOld ? address : AddressValidator.MakeAddress(workchainId, accountId, bounceable, testnetOnly, urlSafe);
         }
+
+		/// <summary>
+		/// Validates address, and returns same address but with 'bounceable' flag to required value.
+		/// </summary>
+		/// <param name="address">Address to validate and convert.</param>
+		/// <param name="bounceable">Required value of 'bounceable' flag.</param>
+		/// <param name="result">Updated address.</param>
+		/// <returns>Returns <b>true</b> if source <paramref name="address"/> is valid (and <paramref name="result"/> is set), and <b>false</b> if <paramref name="address"/> is not valid.</returns>
+        /// <remarks>When source <paramref name="address"/> already have required 'bounceable' flag value - it will be returned unchanged after validation.</remarks>
+		public bool TrySetBounceable(string address, bool bounceable, [NotNullWhen(true)] out string result)
+        {
+            if (!AddressValidator.TryParseAddress(address, out var workchainId, out var accountId, out var bounceableOld, out var testnetOnly, out var urlSafe))
+            {
+                result = string.Empty;
+                return false;
+            }
+
+            result = bounceable == bounceableOld ? address : AddressValidator.MakeAddress(workchainId, accountId, bounceable, testnetOnly, urlSafe);
+            return true;
+        }
     }
 }
