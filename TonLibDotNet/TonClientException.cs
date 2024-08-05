9ce0661aa626da80b85485e2beaea4627f792c3a
Dmitry Popov <dmitry@parsec-club.ru>
2024-08-05 12:58:35 +0300

    feat: remove obsolete ISerializable support from TonClientException

--- TonLibDotNet/TonClientException.cs
+++ TonLibDotNet/TonClientException.cs
@@ -1,10 +1,7 @@
-﻿using System.Runtime.Serialization;
-using TonLibDotNet.Types;
-using TonLibDotNet.Utils;
+﻿using TonLibDotNet.Types;
 
 namespace TonLibDotNet
 {
-    [Serializable]
     public class TonClientException : Exception
     {
         public TonClientException(int code, string message)
@@ -19,27 +16,8 @@ namespace TonLibDotNet
             Code = code;
         }
 
-        protected TonClientException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-            Code = info.GetInt32(nameof(Code));
-
-            var actualAnswerJson = info.GetString(nameof(ActualAnswer));
-            if (!string.IsNullOrEmpty(actualAnswerJson))
-            {
-                ActualAnswer = new TonJsonSerializer().Deserialize(actualAnswerJson);
-            }
-        }
-
         public int Code { get; set; }
 
         public TypeBase? ActualAnswer { get; set; }
-
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            base.GetObjectData(info, context);
-            info.AddValue(nameof(Code), Code);
-            info.AddValue(nameof(ActualAnswer), ActualAnswer == null ? string.Empty : new TonJsonSerializer().Serialize(ActualAnswer));
-        }
     }
 }
