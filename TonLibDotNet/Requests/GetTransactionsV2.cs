9fa67133cf3118312340039283f6dc2698f00746
Mahdi Bagheri <whw1483@gmail.com>
2025-07-23 22:12:25 +0330

    Create GetTransactionsV2.cs
    Added GetTransactoinsV2 request based on https://github.com/ton-blockchain/ton/blob/dfda879419ca3b0173858caa91fbb79b963f4aea/tl/generate/scheme/tonlib_api.tl#L273

---
+++ TonLibDotNet/Requests/GetTransactionsV2.cs
@@ -0,0 +1,26 @@
+﻿using TonLibDotNet.Types;
+
+namespace TonLibDotNet.Requests.Raw
+{
+    [TLSchema("raw.getTransactionsV2 private_key:InputKey account_address:accountAddress from_transaction_id:internal.transactionId count:# try_decode_messages:Bool = raw.Transactions;")]
+    public class GetTransactionsV2 : RequestBase<Types.Raw.Transactions>
+    {
+        public GetTransactions(AccountAddress accountAddress, Types.Internal.TransactionId fromTransactionId, int count = 3, bool tryDecodeMessages = false)
+        {
+            AccountAddress = accountAddress ?? throw new ArgumentNullException(nameof(accountAddress));
+            FromTransactionId = fromTransactionId ?? throw new ArgumentNullException(nameof(fromTransactionId));
+            Count = count;
+            TryDecodeMessages = tryDecodeMessages;
+        }
+
+        public InputKey? PrivateKey { get; set; }
+
+        public AccountAddress AccountAddress { get; set; }
+
+        public Types.Internal.TransactionId FromTransactionId { get; set; }
+
+        public int Count { get; set; }
+
+        public bool TryDecodeMessages { get; set; }
+    }
+}
